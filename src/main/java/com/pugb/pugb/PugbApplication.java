6dd0a7906b6a5a3c1739d1d88b795ab2e3f06525
jose.castillo <jose860304@gmail.com>
2018-08-09 16:40:28 -0400

    My first commit

---
+++ src/main/java/com/pugb/pugb/PugbApplication.java
@@ -0,0 +1,12 @@
+package com.pugb.pugb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PugbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PugbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pugb/pugb/PugbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pugb.pugb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PugbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
