876744a98a9eeeff61055b195c163f26246b4fc9
jose.castillo <jose860304@gmail.com>
2018-08-13 15:11:37 -0400

    Fix Bugs

--- src/main/java/com/pugb/pugb/config/Oauth2ClientAutoConfiguration.java
+++ src/main/java/com/pugb/pugb/config/Oauth2ClientAutoConfiguration.java
@@ -1,14 +1,13 @@
 package com.pugb.pugb.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class Oauth2ClientAutoConfiguration extends WebSecurityEnablerConfiguration{
 	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
          .anyRequest().authenticated()
          .and()
