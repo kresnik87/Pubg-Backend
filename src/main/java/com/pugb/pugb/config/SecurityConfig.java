55b3788a5467c6e7c4f61a7e0a72665e3c133979
jose.castillo <jose860304@gmail.com>
2018-08-13 12:00:52 -0400

    Fix Bugs

--- src/main/java/com/pugb/pugb/config/SecurityConfig.java
+++ src/main/java/com/pugb/pugb/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.pugb.pugb.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,25 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth) 
-      throws Exception {
-        auth.inMemoryAuthentication()
-          .withUser("john").password("123").roles("USER");
-    }
- 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() 
-      throws Exception {
-        return super.authenticationManagerBean();
-    }
- 
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/login").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin().permitAll();
+         .anyRequest().authenticated()
+         .and()
+         .oauth2Login();
     }
 }
