ddf38ab56c2cb1eca6c865ee39316457684a4361
raulhechavarria <raulhperez@gmail.com>
2018-08-20 09:21:57 -0400

    player controller

--- src/main/java/com/pugb/pugb/controllers/PlayerController.java
+++ src/main/java/com/pugb/pugb/controllers/PlayerController.java
@@ -1,13 +1,11 @@
 package com.pugb.pugb.controllers;
 
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.pugb.pugb.controllers.request.ErrorsResquest;
-import com.pugb.pugb.controllers.request.PlayerRequest;
 
 @RestController
 public class PlayerController {
