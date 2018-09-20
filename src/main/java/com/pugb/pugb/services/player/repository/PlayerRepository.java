03e08b8c05a07feb8aa196106c9a36a3bba03ab3
raulhechavarria <raulhperez@gmail.com>
2018-09-20 10:06:10 -0400

    findby fix  to use  just  the name  of other  property

--- src/main/java/com/pugb/pugb/services/player/repository/PlayerRepository.java
+++ src/main/java/com/pugb/pugb/services/player/repository/PlayerRepository.java
@@ -1,10 +1,17 @@
 package com.pugb.pugb.services.player.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pugb.pugb.domain.Player;
 @Repository
 public interface PlayerRepository extends CrudRepository<Player, String> {
+	
+	public List<Player> findByName(String name);
+	
+	public List<Player> findBy(String shardId);
+	
 
 }
--- src/main/java/com/pugb/pugb/services/season/repository/SeasonRepository.java
+++ src/main/java/com/pugb/pugb/services/season/repository/SeasonRepository.java
@@ -1,5 +1,7 @@
 package com.pugb.pugb.services.season.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -16,5 +18,7 @@ public interface SeasonRepository extends CrudRepository<Season, Long>{
 	@Query("SELECT s FROM Season s WHERE s.isCurrentSeason = true")
 	public Season seasonTrue();
 	*/
+	
+	public List<Season> findByType(String type);
 
 }
