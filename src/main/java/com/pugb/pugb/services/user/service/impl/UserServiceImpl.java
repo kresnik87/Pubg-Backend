1b3cecbb626bd07946dc5e9bc752a10367eef071
raulhechavarria <raulhperez@gmail.com>
2018-10-22 17:51:54 -0400

    test

--- src/main/java/com/pugb/pugb/services/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/pugb/pugb/services/user/service/impl/UserServiceImpl.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.pugb.pugb.controllers.request.PlayerRequest;
-import com.pugb.pugb.domain.Player;
 import com.pugb.pugb.domain.User;
 import com.pugb.pugb.services.player.repository.PlayerRepository;
 import com.pugb.pugb.services.user.assembler.UserAssembler;
